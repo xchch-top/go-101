b2631b4853d5e9d23fa5b260cb34a17819afe1ff
_xcc <xcc34569@126.com>
2022-10-15 21:49:18 +0800

    feat(go实战训练营): unsafe和反射两种方式读取字段

---
+++ training/week08/orm_v6/v4/column.go
@@ -0,0 +1,46 @@
+package v4
+
+type Column struct {
+	name string
+}
+
+func C(name string) Column {
+	return Column{name: name}
+}
+
+func (Column) expr() {}
+
+func (Column) selectable() {}
+
+func (Column) assign() {}
+
+// C("id").EQ(12)
+func (c Column) EQ(val any) Predicate {
+	return Predicate{
+		left:  c,
+		op:    opEQ,
+		right: Value{val: val},
+	}
+}
+
+func (c Column) GT(val any) Predicate {
+	return Predicate{
+		left:  c,
+		op:    opGT,
+		right: Value{val: val},
+	}
+}
+
+func (c Column) LT(val any) Predicate {
+	return Predicate{
+		left:  c,
+		op:    opLT,
+		right: Value{val: val},
+	}
+}
+
+type Value struct {
+	val any
+}
+
+func (Value) expr() {}
---
+++ training/week08/orm_v6/v4/error.go
@@ -0,0 +1,7 @@
+package v4
+
+import "gitlab.xchch.top/zhangsai/go-101/training/week08/orm_v6/v4/internal/errs"
+
+var (
+	ErrNoRows = errs.ErrNoRows
+)
