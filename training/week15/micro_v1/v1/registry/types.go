fd64a397d6abe3471f6c95fb2ee8d2593fd07642
_xcc <xcc34569@126.com>
2022-12-02 21:35:25 +0800

    feat(go实战训练营): ResolverNow方法和watch方法单元测试

---
+++ training/week15/micro_v1/v1/registry/types.go
@@ -0,0 +1,36 @@
+package registry
+
+import (
+	"context"
+)
+
+//go:generate mockgen -package=mocks -destination=mocks/registry.mock.go -source=types.go Registry
+type Registry interface {
+	Register(ctx context.Context, ins ServiceInstance) error
+	Unregister(ctx context.Context, ins ServiceInstance) error
+	ListService(ctx context.Context, serviceName string) ([]ServiceInstance, error)
+	Subscribe(serviceName string) (<-chan Event, error)
+	// 可有可无, 不定义的话, 具体的实现也可以额外的添加
+	Close() error
+	// 可以考虑利用ctx来close掉返回的channel
+	// Subscribe(ctx context.Context, serviceName string) (<-chan Event, error)
+}
+
+// ServiceInstance 代表的是一个实例
+type ServiceInstance struct {
+	Address string
+}
+
+type EventType int
+
+const (
+	EventTypeUnknown EventType = iota
+	EventTypeAdd
+	EventTypeDelete
+	EventTypeUpdate
+)
+
+type Event struct {
+	Type     EventType
+	Instance ServiceInstance
+}
