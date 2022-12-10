15bd2044f6cd56db7efe851fad55bc73c4f87f4d
_xcc <xcc34569@126.com>
2022-12-10 15:12:30 +0800

    feat(go实战训练营): 可用性v0代码提交

---
+++ training/week17/micro_v4/v0/example/server/user.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"gitlab.xchch.top/zhangsai/go-101/training/week17/micro_v4/v0/example/proto/gen"
+)
+
+type UserService struct {
+	name string
+	gen.UnimplementedUserServiceServer
+}
+
+func (u *UserService) GetById(ctx context.Context, req *gen.GetByIdReq) (*gen.GetByIdResp, error) {
+	fmt.Printf("server %s, get user id: %d \n", u.name, req.Id)
+	return &gen.GetByIdResp{
+		User: &gen.User{
+			Id:     req.Id,
+			Status: 123,
+		},
+	}, nil
+}
