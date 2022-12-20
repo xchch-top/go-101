d861c46c47fb5657927bc59d37113056513110b7
_xcc <xcc34569@126.com>
2022-12-20 10:55:22 +0800

    feat(go语言第一课): func&errors&panic

--- tonybai/04func_method/panic/c23_v1/panic_recover_test.go
+++ tonybai/04func_method/panic/c23_v1/panic_recover_test.go
@@ -1,10 +1,11 @@
-package main
+package c23_v1
 
 import (
 	"fmt"
+	"testing"
 )
 
-func main() {
+func Test_Panic_Recover(t *testing.T) {
 	fmt.Println("call main")
 	foo2()
 	fmt.Println("exit main")
@@ -16,6 +17,7 @@ func main() {
 // recover the panic:  panic occurs in bar
 // exit foo
 // exit main
+
 func foo2() {
 	fmt.Println("call foo")
 	bar2()
@@ -24,6 +26,9 @@ func foo2() {
 
 func bar2() {
 	defer func() {
+		// recover是go内置的专门用于恢复panic的函数, 它必须被放在一个defer函数中才生效
+		// 如果recover捕捉到panic, 它就会返回以panic的具体内容为错误上下文信息的错误值, 如果没有panic发生, 那么recover将返回nil
+		// 如果panic被recover捕捉到, panic引发的panicking过程就会停止
 		if e := recover(); e != nil {
 			fmt.Println("recover the panic: ", e)
 		}
--- tonybai/04func_method/panic/c23_v1/panic_test.go
+++ tonybai/04func_method/panic/c23_v1/panic_test.go
@@ -1,12 +1,13 @@
-package main
+package c23_v1
 
 import (
 	"fmt"
+	"testing"
 )
 
 // 当函数F调用panic函数时, 函数F的执行将停止
 // 不过函数F中已进行的deferred函数都会正常执行, 执行完这些deferred函数后, 函数F才会把控制权返还给其调用者
-func main() {
+func Test_Panic_V1(t *testing.T) {
 	fmt.Println("call main")
 	foo()
 	fmt.Println("exit main")
@@ -16,6 +17,7 @@ func main() {
 // call foo
 // call bar
 // panic: panic occurs in bar
+
 func foo() {
 	fmt.Println("call foo")
 	bar()
